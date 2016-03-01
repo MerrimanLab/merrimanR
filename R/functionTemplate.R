bd72f68da983b31ebe376840fe82fb0e448d63a7
murraycadzow <murray.cadzow@otago.ac.nz>
2016-03-01 16:15:55 +1300

    move cursor to inside function body if file has been saved

--- R/functionTemplate.R
+++ R/functionTemplate.R
@@ -5,5 +5,11 @@
 #' @export
 
 functionTemplate <- function() {
-  rstudioapi::insertText(" <- function() {\n\n} ")
+  rstudioapi::insertText(" <- function() {\n\t\n} ")
+  if(rstudioapi::getActiveDocumentContext()$path != ""){
+    rstudioapi::navigateToFile(file = rstudioapi::getActiveDocumentContext()$path,
+                               line = rstudioapi::getActiveDocumentContext()$selection[[1]]$range$start[1] -1 ,
+                               column= 3)
+  }
 }
+
