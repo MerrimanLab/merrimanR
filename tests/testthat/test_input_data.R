fda49df7a426127125d09da5a0af5d6ce5d66596
murraycadzow <murray.cadzow@otago.ac.nz>
2016-03-02 15:30:30 +1300

    continue to add clauses to make fail

--- tests/testthat/test_input_data.R
+++ tests/testthat/test_input_data.R
@@ -2,9 +2,23 @@ library('merrimanR')
 context("input checking")
 
 not_df <- matrix()
+df <- data.frame()
+
+f <- data.frame(V1 = c(3,5,1), V1 = c("C8","A6","A2"), V3 = c(0,0,0), V4 = c(0,0,0), V5 = c(0,0,0), V6 = c(-9,-9,-9))
+p <- data.frame(IID = c("A2", "A6", "B7", "C2"), trait = c(1,1,2,3))
+test_that("check arguments are provided",{
+  expect_error(remove_empty_cols(), "Need to provide data.frame")
+  expect_error(update_fam(), "Make sure all arugments are filled")
+})
 
 test_that("check correct input data structure",{
-  expect_error(remove_empty_cols(), "Need to provide data.frame")
   expect_error(remove_empty_cols(not_df), "Need to use a data.frame")
+
+  expect_error(update_fam(fam = not_df, pheno= df, iid_col = "",pheno_col = "" ), "fam and pheno_df need to be data.frame")
+  expect_error(update_fam(fam = df, pheno= not_df, iid_col = "",pheno_col = "" ), "fam and pheno_df need to be data.frame")
+  expect_error(update_fam(fam = not_df, pheno= not_df, iid_col = "",pheno_col = "" ), "fam and pheno_df need to be data.frame")
+  expect_error(update_fam(fam = df, pheno = df, iid_col = 1, pheno_col = ""), "id_col and pheno_col need to be type character")
+  expect_error(update_fam(fam = df, pheno = df, iid_col = "", pheno_col = 1), "id_col and pheno_col need to be type character")
+  expect_error(update_fam(fam = f, pheno_df = p, iid_col = "IID", pheno_col = "trait"), "Ensure column names of fam are 'FID','IID', 'PID','MID','SEX','AFF'")
 })
 
--- tests/testthat/test_outputs.R
+++ tests/testthat/test_outputs.R
@@ -8,7 +8,10 @@ test_that("check correct output remove_empty_cols()",{
   expect_identical(remove_empty_cols(df), data.frame(col2 = c(1,1,NA), col3 = c(NA,1,NA) ))
 })
 
-
-
+f <- data.frame(FID = c(3,5,1), IID = c("C8","A6","A2"), PID = c(0,0,0), MID = c(0,0,0), SEX = c(0,0,0), AFF = c(-9,-9,-9))
+p <- data.frame(IID = c("A2", "A6", "B7", "C2"), trait = c(1,1,2,3))
+test_that("output fam dimensions are correct",{
+  expect_identical(dim(update_fam(fam = f, pheno_df = p, iid_col = "IID", pheno_col = "trait")), dim(f))
+})
 
 
